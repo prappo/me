c3850209699257aa824c13c79a348cc188924046
Prappo Prince <prappo.prince@gmail.com>
2021-07-04 21:28:34 +0600

    added gist

---
+++ dist/_nuxt/14aa6bd.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{217:function(t,e,n){"use strict";n.r(e);var c=n(34),component=Object(c.a)({},(function(){var t=this,e=t.$createElement;return(t._self._c||e)("section",{staticClass:"bg-blue-100",attrs:{id:"worksSection"}},[t._v("Works Section")])}),[],!1,null,null,null);e.default=component.exports}}]);
