7dc853d13abe55c980ad8e47255ad9fa774d6168
Prappo Prince <prappo.prince@gmail.com>
2023-10-28 07:04:06 +0000

    add dist folder

---
+++ dist/_nuxt/dcadc3c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{275:function(n,t,l){"use strict";l.r(t);var e=l(44),component=Object(e.a)({},(function(){return(0,this._self._c)("div",[this._v("\n    Blog\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
